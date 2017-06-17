e7328ca3208a3fe8ec7c54b97e4283861f49ce45
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-17 12:44:43 -0400

    function to get coordinates for all buildings in a zone

--- analysis.js
+++ analysis.js
@@ -1,5 +1,7 @@
-function getCrimeAmountWithinRange(lowLat,highLat,lowLong,highLong,callback) {
+const query = require('./query');
 
+function getCrimeAmountWithinRange(lowLat,highLat,lowLong,highLong,callback) {
+    
 }
 
 function getCrimeAmountWithinRangeWithinYear(lowLat,highLat,lowLong,highLong,callback) {
@@ -10,8 +12,29 @@ function getTotalCrimeAmountByYear(year,callback) {
 
 }
 
-function getCrimeChartData(callback) {
+function getCrimeChartData() {
+    return([53917,53017,52713,55757]);
+}
 
+function getCoordinatesForZone(zone,callback) {
+    query.queryData(`
+        SELECT address_cords.address, address_cords.latitude, address_cords.longitude, raw_land_use.use14_label
+        FROM raw_land_use 
+        INNER JOIN address_cords
+        ON address_cords.address = CONCAT(CAST(raw_land_use.stnumber AS VARCHAR), ' ', raw_land_use.full_stname)
+        AND raw_land_use.use14_label = '${zone}';
+    `,function(err,results) {
+        let coordinateData = [];
+        results.shift();
+        results.forEach(function(result) {
+            let coordinate = {
+                lat: result.Data[1].VarCharValue,
+                long: result.Data[2].VarCharValue,
+            }
+            coordinateData.push(coordinate);
+        })
+        callback(err,coordinateData);
+    });
 }
 
 function getCrimeLatLongWithinRange(lowLat,highLat,lowLong,highLong,callback) {
@@ -28,3 +51,4 @@ module.exports.getTotalCrimeAmountByYear = getTotalCrimeAmountByYear;
 module.exports.getCrimeChartData = getCrimeChartData;
 module.exports.getCrimeLatLongWithinRange = getCrimeLatLongWithinRange;
 module.exports.getCrimeLatLongWithinRangeWithinYear = getCrimeAmountWithinRangeWithinYear;
+module.exports.getCoordinatesForZone = getCoordinatesForZone;
--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const query = require('./query');
-
+const analysis = require('./analysis');
+/*
 query.queryData('SELECT * FROM raw_land_use LIMIT 10;',function(err,results) {
     if(err) console.log(err,err.stack);
     else {
         console.log(JSON.stringify(results));
     }
+});
+(?)
+*/
+
+analysis.getCoordinatesForZone('Light Industrial',function(err,results) {
+    console.log(JSON.stringify(results));
+    
 });
