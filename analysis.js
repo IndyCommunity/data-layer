70363318d6cd469cc56c5fc423ae56b11d54b80e
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-17 10:44:42 -0400

    module export and more method definitionss

---
+++ analysis.js
@@ -0,0 +1,30 @@
+function getCrimeAmountWithinRange(lowLat,highLat,lowLong,highLong) {
+
+}
+
+function getCrimeAmountWithinRangeWithinYear(lowLat,highLat,lowLong,highLong) {
+
+}
+
+function getTotalCrimeAmountByYear(year) {
+
+}
+
+function getCrimeChartData() {
+
+}
+
+function getCrimeLatLongWithinRange(lowLat,highLat,lowLong,highLong) {
+
+}
+
+function getCrimeLatLongWithinRangeWithinYear(lowLat,highLat,lowLong,highLong) {
+
+}
+
+module.exports.getCrimeAmountWithinRange = getCrimeAmountWithinRange;
+module.exports.getCrimeAmountWithinRangeWithinYear = getCrimeAmountWithinRangeWithinYear;
+module.exports.getTotalCrimeAmountByYear = getTotalCrimeAmountByYear;
+module.exports.getCrimeChartData = getCrimeChartData;
+module.exports.getCrimeLatLongWithinRange = getCrimeLatLongWithinRange;
+module.exports.getCrimeLatLongWithinRangeWithinYear = getCrimeAmountWithinRangeWithinYear;
