477457d784045b74c3df2cc94e2c809242e69624
Andrew Arpasi <andrew@andrewarpasi.com>
2017-06-17 10:42:31 -0400

    add some method defenitions for queries

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const query = require('./query');
 
-query.queryData('SELECT * FROM raw_crime LIMIT 10;',function(err,results) {
+query.queryData('SELECT * FROM raw_land_use LIMIT 10;',function(err,results) {
     if(err) console.log(err,err.stack);
     else {
         console.log(JSON.stringify(results));
